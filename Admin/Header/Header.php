18310c1ec33cf3aeec05ca44650679f63bde4207
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-07-15 23:04:44 +0600

    Create Header.php

---
+++ Admin/Header/Header.php
@@ -0,0 +1,22 @@
+<?php session_start();
+		
+	include("connection.php");
+
+?>
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+		<link href="style.css" rel="stylesheet">
+		<title> Admin Panel </title>
+		
+	</head>
+	<div class="head">
+		<h1 style="color: Black; text-align: center;"> Welcome to Admin Panel </h1>
+		
+		<div class="loginfo">
+		
+			<h3> <a href="AdminLogout.php">Logout</a></h3>
+		
+		</div>	
+	</div>
