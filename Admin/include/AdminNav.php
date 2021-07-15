96a49538ed504e247859c5834ff55b6ebb847148
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-07-15 23:07:52 +0600

    Add files via upload

---
+++ Admin/include/AdminNav.php
@@ -0,0 +1,25 @@
+
+<div style="background-color: white;">
+
+	<link href="style.css" rel="stylesheet">
+	
+	<div class="menu">
+		<ul>
+			<li><a href="Admin.php">Home</a></li>
+			
+			<li class="dropdown">
+				<a href="javascript:void(0)" class="dropbtn">Event</a>
+				<div class="dropdown-content">
+					<a href="Category.php?cat=Cricket"> Cricket </a>
+					<a href="Category.php?cat=Football"> Football </a>
+					<a href="Category.php?cat=Tennis"> Tennis </a>
+					<a href="Category.php?cat=Hockey"> Hockey </a>
+				</div>
+			</li>
+			
+			<li><a href="Add.php">Add Event</a></li>
+			
+		</ul>
+	</div>
+	
+</div>
